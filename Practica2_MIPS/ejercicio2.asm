ed75b054cfdb311751449b447863d6fe8e9c2d1b
gonzacaser <gonzacasermeiro@gmail.com>
2022-09-20 15:17:05 -0300

    Segundo Ejercicio corregido, terminado

--- Practica2_MIPS/ejercicio2.asm
+++ Practica2_MIPS/ejercicio2.asm
@@ -9,9 +9,9 @@ res:        .space  3                       # reservo espacio para guardar resul
             lw $t1, V + 4($0)               # sumo de a 4 porque cada palabra ocupa 4 bytes
             lw $t2, V + 8($0)
 
-            sge $t3, $t0, $0                # pongo 1 en t3 si t0 > 0, sino pongo 0
-            sge $t4, $t1, $0                # pongo 1 en t4 si t1 > 0, sino pongo 0
-            sge $t5, $t2, $0                # pongo 1 en t5 si t2 > 0, sino pongo 0
+            sge $t3, $t0, $0                # pongo 1 en t3 si t0 >= 0, sino pongo 0
+            sge $t4, $t1, $0                # pongo 1 en t4 si t1 >= 0, sino pongo 0
+            sge $t5, $t2, $0                # pongo 1 en t5 si t2 >= 0, sino pongo 0
 
             sb $t3, res($0)                 # guardo bytes 0 o 1 en res
             sb $t4, res + 1($0)
